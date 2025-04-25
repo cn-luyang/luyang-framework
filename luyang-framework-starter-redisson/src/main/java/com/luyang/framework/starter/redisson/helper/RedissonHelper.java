a77aa0faa45f0f94187a6c62adb27a1b2be2eb71
luyang <cn_luyang@163.com>
2025-04-25 23:47:55 +0900

    no message

--- luyang-framework-starter-redisson/src/main/java/com/luyang/framework/starter/redisson/helper/RedissonHelper.java
+++ luyang-framework-starter-redisson/src/main/java/com/luyang/framework/starter/redisson/helper/RedissonHelper.java
@@ -1,11 +1,7 @@
 package com.luyang.framework.starter.redisson.helper;
 
-import org.redisson.api.RedissonClient;
-
 /**
  * @author yang.lu
  */
 public class RedissonHelper {
-
-	private RedissonClient redissonClient;
 }
