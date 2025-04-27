a313bfdb7c5108502dc6448e57c0d8a3e223d6ec
LU YANG <cn_luyang@163.com>
2025-04-28 04:47:39 +0900

    no message

--- luyang-framework-starter-redisson/src/main/java/com/luyang/framework/starter/redisson/config/RedissonConfiguration.java
+++ luyang-framework-starter-redisson/src/main/java/com/luyang/framework/starter/redisson/config/RedissonConfiguration.java
@@ -2,12 +2,18 @@ package com.luyang.framework.starter.redisson.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.luyang.framework.starter.base.util.SpringUtil;
+import com.luyang.framework.starter.redisson.config.properties.RedissonProperties;
+import com.luyang.framework.starter.redisson.helper.RedissonHelper;
 import com.luyang.framework.starter.redisson.support.RedissonNameMapper;
+import org.redisson.api.RedissonClient;
 import org.redisson.codec.JsonJacksonCodec;
 import org.redisson.spring.starter.RedissonAutoConfigurationCustomizer;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Optional;
 
 /**
  * Redisson 配置
@@ -17,15 +23,24 @@ import org.springframework.context.annotation.Configuration;
  */
 @AutoConfigureAfter(ObjectMapper.class)
 @Configuration(proxyBeanMethods = false)
+@EnableConfigurationProperties(RedissonProperties.class)
 public class RedissonConfiguration {
 
 	@Bean
-	public RedissonAutoConfigurationCustomizer redissonAutoConfigurationCustomizer() {
+	public RedissonHelper redissonHelper(RedissonClient redissonClient) {
+		return new RedissonHelper(redissonClient);
+	}
+
+	@Bean
+	public RedissonAutoConfigurationCustomizer redissonAutoConfigurationCustomizer(RedissonProperties redissonProperties) {
 		return config -> {
 			// 设置 JSON 编解码器
 			config.setCodec(new JsonJacksonCodec(SpringUtil.getBean(ObjectMapper.class)));
 
-			String keyPrefix = SpringUtil.getApplicationName().toUpperCase();
+			String keyPrefix = Optional.ofNullable(redissonProperties.getPrefix())
+				.map(String::toUpperCase)
+				.orElse(SpringUtil.getApplicationName().toUpperCase());
+
 			RedissonNameMapper redissonNameMapper = new RedissonNameMapper(keyPrefix);
 
 			// 单机模式
--- luyang-framework-starter-redisson/src/main/java/com/luyang/framework/starter/redisson/config/properties/RedissonProperties.java
+++ luyang-framework-starter-redisson/src/main/java/com/luyang/framework/starter/redisson/config/properties/RedissonProperties.java
@@ -1,7 +1,20 @@
 package com.luyang.framework.starter.redisson.config.properties;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
 /**
+ * Redisson 配置属性
+ *
  * @author yang.lu
  */
+@Getter
+@Setter
+@ConfigurationProperties(prefix = RedissonProperties.PREFIX)
 public class RedissonProperties {
+
+	public static final String PREFIX = "spring.data.redis";
+
+	private String prefix;
 }
