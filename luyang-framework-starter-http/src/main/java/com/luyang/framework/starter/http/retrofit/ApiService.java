ab8e0125f40e248010c7eb27e7e40f544602171c
wangjixin <398022743@qq.com>
2025-04-28 13:48:34 +0800

    fix:http工具类

---
+++ luyang-framework-starter-http/src/main/java/com/luyang/framework/starter/http/retrofit/ApiService.java
@@ -0,0 +1,33 @@
+package com.luyang.framework.starter.http.retrofit;
+
+import okhttp3.MultipartBody;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.*;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * @author wangjixin
+ */
+public interface ApiService {
+	@GET("/users/{id}")
+	Call<Object> getUser(@Path("id") String userId);
+
+	@Multipart
+	@POST("/upload")
+	Call<ResponseBody> uploadFile(
+		@Part("description") RequestBody description,
+		@Part MultipartBody.Part file
+	);
+
+	public static ApiService create() {
+		return new Retrofit.Builder()
+			.baseUrl("https://api.example.com/")
+			.addConverterFactory(GsonConverterFactory.create())
+			.build()
+			.create(ApiService.class);
+	}
+}
+
