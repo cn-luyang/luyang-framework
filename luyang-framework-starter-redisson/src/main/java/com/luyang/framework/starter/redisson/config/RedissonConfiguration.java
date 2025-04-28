4f968516dac3c211c260d7d3e531269e1cfa2820
LU YANG <cn_luyang@163.com>
2025-04-28 09:50:10 +0900

    no message

--- luyang-framework-starter-redisson/src/main/java/com/luyang/framework/starter/redisson/config/RedissonConfiguration.java
+++ luyang-framework-starter-redisson/src/main/java/com/luyang/framework/starter/redisson/config/RedissonConfiguration.java
@@ -1,15 +1,15 @@
 package com.luyang.framework.starter.redisson.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.luyang.framework.starter.base.util.SpringUtil;
 import com.luyang.framework.starter.redisson.config.properties.RedissonProperties;
 import com.luyang.framework.starter.redisson.helper.RedissonHelper;
 import com.luyang.framework.starter.redisson.support.RedissonNameMapper;
+import lombok.RequiredArgsConstructor;
 import org.redisson.api.RedissonClient;
 import org.redisson.codec.JsonJacksonCodec;
 import org.redisson.spring.starter.RedissonAutoConfigurationCustomizer;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,10 +21,14 @@ import java.util.Optional;
  *
  * @author yang.lu
  */
-@AutoConfigureAfter(ObjectMapper.class)
+@RequiredArgsConstructor
 @Configuration(proxyBeanMethods = false)
 @EnableConfigurationProperties(RedissonProperties.class)
 public class RedissonConfiguration {
+
+	private final ObjectMapper objectMapper;
+	private final RedissonProperties redissonProperties;
+	private final ApplicationContext applicationContext;
 
 	@Bean
 	public RedissonHelper redissonHelper(RedissonClient redissonClient) {
@@ -32,14 +36,13 @@ public class RedissonConfiguration {
 	}
 
 	@Bean
-	public RedissonAutoConfigurationCustomizer redissonAutoConfigurationCustomizer(RedissonProperties redissonProperties) {
+	public RedissonAutoConfigurationCustomizer redissonAutoConfigurationCustomizer() {
 		return config -> {
 			// 设置 JSON 编解码器
-			config.setCodec(new JsonJacksonCodec(SpringUtil.getBean(ObjectMapper.class)));
-
+			config.setCodec(new JsonJacksonCodec(objectMapper));
 			String keyPrefix = Optional.ofNullable(redissonProperties.getPrefix())
 				.map(String::toUpperCase)
-				.orElse(SpringUtil.getApplicationName().toUpperCase());
+				.orElse(applicationContext.getEnvironment().getProperty("spring.application.name"));
 
 			RedissonNameMapper redissonNameMapper = new RedissonNameMapper(keyPrefix);
 
