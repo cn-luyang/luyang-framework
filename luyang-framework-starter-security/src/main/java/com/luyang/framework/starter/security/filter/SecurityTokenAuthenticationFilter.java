ac2c77d0bea3ce726cb4972d427ae2e4221eb77e
luyang <cn_luyang@163.com>
2025-05-11 02:10:07 +0900

    no message

--- luyang-framework-starter-security/src/main/java/com/luyang/framework/starter/security/filter/SecurityTokenAuthenticationFilter.java
+++ luyang-framework-starter-security/src/main/java/com/luyang/framework/starter/security/filter/SecurityTokenAuthenticationFilter.java
@@ -1,5 +1,7 @@
 package com.luyang.framework.starter.security.filter;
 
+import cn.hutool.core.map.MapUtil;
+import cn.hutool.core.util.IdUtil;
 import cn.hutool.core.util.StrUtil;
 import com.luyang.framework.starter.security.util.SecurityUtil;
 import jakarta.servlet.FilterChain;
@@ -8,9 +10,15 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Token 认证过滤器
@@ -25,13 +33,21 @@ public class SecurityTokenAuthenticationFilter extends OncePerRequestFilter {
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
 		throws ServletException, IOException {
 
-		String authToken = SecurityUtil.getTokenValue(request);
-		if (StrUtil.isBlank(authToken)) {
-			chain.doFilter(request, response);
-			return;
-		}
+//		String authToken = SecurityUtil.getTokenValue(request);
+//		if (StrUtil.isBlank(authToken)) {
+//			chain.doFilter(request, response);
+//			return;
+//		}
 
 		// TODO: 远程调用认证服务、验证token是否有效返回用户信息
+		Map<String, String> map = MapUtil.newHashMap();
+		map.put("id", "123");
+		map.put("name", "luyang");
+
+		UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+			map, null, Collections.emptyList());
+		authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+		SecurityContextHolder.getContext().setAuthentication(authenticationToken);
 
 		chain.doFilter(request, response);
 	}
