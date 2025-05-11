fec91b367b463516b549da1a8d609c0f2f3cb815
LU YANG <cn_luyang@163.com>
2025-05-12 05:58:24 +0900

    no message

--- luyang-framework-starter-security/src/main/java/com/luyang/framework/starter/security/filter/SecurityTokenAuthenticationFilter.java
+++ luyang-framework-starter-security/src/main/java/com/luyang/framework/starter/security/filter/SecurityTokenAuthenticationFilter.java
@@ -1,9 +1,6 @@
 package com.luyang.framework.starter.security.filter;
 
 import cn.hutool.core.map.MapUtil;
-import cn.hutool.core.util.IdUtil;
-import cn.hutool.core.util.StrUtil;
-import com.luyang.framework.starter.security.util.SecurityUtil;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -17,7 +14,6 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
 
 /**
--- luyang-test-demo/src/main/java/com/luyang/test/demo/DemoApplication.java
+++ luyang-test-demo/src/main/java/com/luyang/test/demo/DemoApplication.java
@@ -1,5 +1,7 @@
 package com.luyang.test.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,7 +11,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		logger.error("错误日志");
 	}
 }
