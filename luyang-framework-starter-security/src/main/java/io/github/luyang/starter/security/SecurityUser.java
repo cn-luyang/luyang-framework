de0509655508de9592d9a1d715b3aaf8cc1b895b
luyang <cn_luyang@163.com>
2025-06-23 21:39:04 +0800

    no message

--- luyang-framework-starter-security/src/main/java/io/github/luyang/starter/security/SecurityUser.java
+++ luyang-framework-starter-security/src/main/java/io/github/luyang/starter/security/SecurityUser.java
@@ -3,15 +3,20 @@ package io.github.luyang.starter.security;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 /**
  * @author yang.lu
  */
 @Getter
 @Setter
-public class SecurityUser {
+public class SecurityUser implements Serializable {
+
+	@Serial
+	private static final long serialVersionUID = 4758668578675934182L;
 
 	private String clientId;
-	private String clientName;
 	private String userId;
 	private String zhName;
 	private String enName;
