a14227c78e2359bf139bc362182faf6f94cc20e8
luyang <cn_luyang@163.com>
2025-08-11 00:10:59 +0800

    no message

--- luyang-framework-starter-web/src/main/java/io/github/luyang/starter/web/config/WebConfiguration.java
+++ luyang-framework-starter-web/src/main/java/io/github/luyang/starter/web/config/WebConfiguration.java
@@ -1,10 +1,14 @@
 package io.github.luyang.starter.web.config;
 
+import io.github.luyang.starter.web.core.advice.GlobalExceptionAdvice;
 import io.github.luyang.starter.web.core.filter.TraceIdFilter;
+import io.github.luyang.starter.web.core.initializer.BannerInitializer;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.Ordered;
+import org.springframework.core.env.Environment;
 
 /**
  * Web 相关配置类
@@ -21,5 +25,15 @@ public class WebConfiguration {
 		registrationBean.addUrlPatterns("/*");
 		registrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
 		return registrationBean;
+	}
+
+	@Bean
+	public GlobalExceptionAdvice globalExceptionAdvice() {
+		return new GlobalExceptionAdvice();
+	}
+
+	@Bean
+	public BannerInitializer bannerInitializer(Environment environment, ApplicationContext context) {
+		return new BannerInitializer(environment, context);
 	}
 }
