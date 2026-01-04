c08d957cc3a1a4a239e7f1b7bcb6c063d25a0e36
luyang <cn_luyang@163.com>
2026-01-04 13:14:43 +0800

    增加IRedisKeyEnum接口类，规范化Redis 键名

---
+++ luyang-framework-starter-redisson/src/main/java/io/github/luyang/starter/redisson/IRedisKeyEnum.java
@@ -0,0 +1,51 @@
+package io.github.luyang.starter.redisson;
+
+import java.io.Serializable;
+import java.util.StringJoiner;
+
+/**
+ * Redis 键名规范化枚举接口
+ *
+ * @author yang.lu
+ */
+public interface IRedisKeyEnum extends Serializable {
+
+	/**
+	 * 获取 Key 前缀
+	 */
+	String prefix();
+
+	/**
+	 * 获取过期时间（单位：秒）
+	 */
+	long expireSeconds();
+
+	/**
+	 * 获取业务描述
+	 */
+	String getDescription();
+
+	/**
+	 * 构建完整的 Redis Key
+	 *
+	 * @param args 动态占位参数（将以冒号分割拼接）
+	 * @return 完整 Key，例如 "prefix:arg1:arg2"
+	 */
+	default String buildKey(Object... args) {
+		if (null == args || args.length == 0) {
+			return prefix();
+		}
+
+		if (args.length == 1) {
+			return prefix() + ":" + args[0];
+		}
+
+		StringJoiner joiner = new StringJoiner(":");
+		joiner.add(prefix());
+		for (Object arg : args) {
+			joiner.add(String.valueOf(arg));
+		}
+
+		return joiner.toString();
+	}
+}
